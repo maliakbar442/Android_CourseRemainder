9b0b7611bda44f946291f675f6638f7ddf622516
akbar <muhammad.akbar@mncgroup.com>
2021-09-02 09:35:12 +0700

    Adding CourseRemainder

---
+++ CourseRemainder/app/src/main/java/com/example/courseremainder/Features/UpdateStudentInfo/StudentUpdateListener.java
@@ -0,0 +1,7 @@
+package com.example.courseremainder.Features.UpdateStudentInfo;
+
+import com.example.courseremainder.Features.CreateStudent.Student;
+
+public interface StudentUpdateListener {
+    void onStudentInfoUpdated(Student student, int position);
+}
